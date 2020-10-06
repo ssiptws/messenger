4171304e42931dae8ec0fbe0e6de05de4027aedc
wickedr0se <32614215+wickedr0se@users.noreply.github.com>
2020-10-07 06:41:42 +0700

    login page tweak + connect db

---
+++ messenger/login.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" href="stylelogin.css" type="text/css">
+</head>
+<body style="background-color: #2980b9">
+    <div style= "margin: 10%">
+        <h2>Login</h2> 
+        <form action="" method="post">
+        <div class="container">
+            <label><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+            <label><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required> 
+            <button type="submit">Masuk</button>
+            <input type="checkbox" checked="checked"><span> Ingat Saya</span>
+        </div>
+        <div class="container" style="background-color: #13232f; margin-top: 10px">
+			<a href="register.php" class="regisbtn regislink">Registration</a>
+            <a href="forgot.php" class="forgot">Lupa Password?</a>
+        </div>
+        </form>
+    </div>
+</body>
+</html>
