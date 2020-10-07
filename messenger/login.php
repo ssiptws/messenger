5ef7a91e49da338b9006c6ee3ecb3532797ba58b
wickedr0se <32614215+wickedr0se@users.noreply.github.com>
2020-10-07 11:02:47 +0700

    working backend

--- messenger/login.php
+++ messenger/login.php
@@ -1,3 +1,66 @@
+<?php
+	session_start();
+	if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+		header("location: index.php");
+		exit;
+	}
+	require_once "connect.php";
+	$username = $password = "";
+	$username_err = $password_err = "";
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		if(empty(trim($_POST["username"]))){
+			$username_err = "Please enter username";
+		}
+		else{
+			$username = trim($_POST["username"]);
+		}
+		
+		if(empty(trim($_POST["password"]))){
+			$password_err = "Please enter password";
+		}
+		else{
+			$password = trim($_POST["password"]);
+		}
+		
+		if(empty($username_err) && empty($password_err)){
+			$sql = "SELECT id, username, password FROM users WHERE username = ?";
+			if($stmt = mysqli_prepare($connect, $sql){
+				mysqli_stmt_bind_param($stmt, "s", $param_username);
+				$param_username = $username;
+				
+				if(mysqli_stmt_execute($stmt)){
+					mysqli_stmt_store_result($stmt);
+					
+					if(mysqli_stmt_num_rows($stmt) == 1){
+						mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+						if(mysqli_stmt_fetch($stmt){
+							if(password_verify($password, $hashed_password){
+								session_start();
+								$_SESSION["loggedin"] = true;
+								$_SESSION["id"] = $id;
+								$_SESSION["username"] = $username;
+								header("location: index.php");
+							}
+							else{
+								$password_err = "Wrong password";
+							}
+						}
+					}
+					else{
+						$username_err = "No account";
+					}
+				}
+				else{
+					echo "something went wrong.";
+				}
+			}
+		}
+		mysqli_stmt_close($stmt);
+	}
+?>
+<!-- 
+	TODO: TWEAK LOGIN PAGE
+!-->
 <html>
 <head>
 	<title>Login</title>
