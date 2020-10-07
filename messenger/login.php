8dadcfafe3d7c75307dc880eb4a39bdca2591a31
wirahmat <58240412+wirahmat@users.noreply.github.com>
2020-10-07 14:55:42 +0700

    minor change
    ubah nama container biar ngga tabrakan

--- messenger/login.php
+++ messenger/login.php
@@ -70,7 +70,7 @@
     <div style= "margin: 10%">
         <h2>Login</h2> 
         <form action="" method="post">
-        <div class="container">
+        <div class="containerlogin">
             <label><b>Username</b></label>
             <input type="text" placeholder="Enter Username" name="uname" required>
             <label><b>Password</b></label>
@@ -78,7 +78,7 @@
             <button type="submit">Masuk</button>
             <input type="checkbox" checked="checked"><span> Ingat Saya</span>
         </div>
-        <div class="container" style="background-color: #25333D; margin-top: 10px">
+        <div class="containerlogin" style="background-color: #25333D; margin-top: 10px">
 			<a href="register.php" class="regisbtn regislink">Registration</a>
             <a href="forgot.php" class="forgot">Lupa Password?</a>
         </div>
