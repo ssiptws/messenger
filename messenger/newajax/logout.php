539aabe0d8ddf5be3c837372802bbdcd297a0061
wickedr0se <32614215+wickedr0se@users.noreply.github.com>
2020-10-13 09:08:19 +0700

    Create logout.php

---
+++ messenger/newajax/logout.php
@@ -0,0 +1,11 @@
+<?php
+	session_start();
+	include ('Chat.php');
+	$chat = new Chat();
+	$chat -> updateUserOnline($_SESSION['userid'], 0);
+	$_SESSION['username'] = "";
+	$_SESSION['userid']  = "";
+	$_SESSION['login_details_id']= "";
+	header("location: login.php");
+	exit;
+?>
