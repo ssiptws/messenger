4c9ae1b6f0d3ef0aad20222956d90fb593b40543
wickedr0se <32614215+wickedr0se@users.noreply.github.com>
2020-10-15 19:08:36 +0700

    major update

--- messenger/logout.php
+++ messenger/logout.php
@@ -1,7 +1,11 @@
 <?php
-	session_start();
-	$_SESSION = array();
-	session_destroy();
-	header("location: login.php");
-	exit;
+
+//logout.php
+
+session_start();
+
+session_destroy();
+
+header('location:login.php');
+
 ?>
